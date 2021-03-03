944e0899c3a1aea936358236a91e48cfde82e12a
Ronald <ronald.patrick1101@gmail.com>
2021-03-03 20:44:09 +0530

    Completed Code

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    r=document.getElementById("rate").value;
+    y=document.getElementById("years").value;
+    res=document.getElementById('result');
+    let interest=(p*r*y)/100;
+    console.log(res);
+    res.innerHTML=interest;
 }
         
